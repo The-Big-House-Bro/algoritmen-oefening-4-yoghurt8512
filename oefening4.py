46312459ee0c335571e295a86af5bf551595c1db
youssef khawam <khawamyyy@gmail.com>
2025-03-27 21:41:05 +0100

    oef 4 klaar

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,14 @@
-def countTargetPairs():
+target = int(input("input target"))
+nums = list(map(int, input("lijst").split(",")))
+def GetallenMetParen(nums, target):
+    
+    TrPairs = 0
+    for i in range(len(nums)):
+        for j in range( i +1, len(nums)):
+            if i != j:
+                if nums[i] + nums[j] < target:
+                    TrPairs += 1
+    return TrPairs
+
+
+print(GetallenMetParen(nums, target))
